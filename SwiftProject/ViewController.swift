6a37af1507a15f276503449f3a20c1afc9b7f4ee
邵爱茹 <airu.shao@irootech.com>
2018-06-13 07:27:19 +0800

    first commit

--- SwiftProject/ViewController.swift
+++ SwiftProject/ViewController.swift
@@ -13,8 +13,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let myString = "Hello,World!"
+        print(myString)
+        
+        typealias Feet = Int
+        let distance:Feet = 100
+        print(distance)
+        
+        let varA = 42
+        print(varA)
+        
+        var varB:Float
+        varB = 3.1415926
+        print(varB)
+        
+        var name = "菜鸟教程"
+        var site = "http://www.runoob.com"
+        
+        print("\(name)的官网地址为：\(site)")
+        //可选类型
+        var optionInteger1:Int?
+        var optionInteger2:Optional<Int>
+        var optionInteger3:([Int])?
+        
     }
 
+    /*多行注释的开头
+     /*
+     嵌套的多行注释
+     */
+     多行注释的结尾*/
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
